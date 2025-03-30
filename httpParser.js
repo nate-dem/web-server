5b57a859aa0bcf6844d9d38d32cc737f24b89646
nate-dem <natedem@stanford.edu>
2025-03-29 23:53:54 -0700

    wrote basic TCP server and http parsing function

---
+++ httpParser.js
@@ -0,0 +1,27 @@
+function httpParser(text) {
+    const res = {}
+
+    const mainSections = text.split("\r\n\r\n");
+    const header = mainSections[0];
+    const body = mainSections[1];
+
+    const headerSections = header.split("\r\n");
+    const request = headerSections[0];
+
+    const [method, path, version] = request.split(" ");
+    res.method = method;
+    res.path = path;
+    res.version = version;
+    res.headers = {};
+
+    for (let line of headerSections.slice(1)) {
+        const [key, value] = line.split(": ");
+        res.headers[key.toLowerCase()] = value.trim();
+    };
+
+    if (res.method == "POST") {
+        res.body = body;
+    };
+
+    return res;
+};
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// main server file
+const net = require("net")
+
+function newConn(socket) {
+    console.log('new connection', socket.remoteAddress, socket.remotePort);
+
+    socket.on('end', () => {
+        console.log('EOF.');
+    });
+    socket.on('data', (data) => {
+        const text = data.toString();
+        console.log("Original text: ", text);
+
+        const response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, world!";
+        socket.write(response);
+
+        socket.end();
+    });
+}
+
+let server = net.createServer();
+server.on('error', (err) => { throw err; });
+server.on('connection', newConn);
+server.listen({host: '127.0.0.1', port: 1234});
