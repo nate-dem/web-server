33d32138f80de5d6b78bf1a05dbd2c80c942f9c9
nate-dem <natedem@stanford.edu>
2025-03-30 15:21:55 -0700

    routing functionality which will be used for my personal portfolio

--- responseBuilder.js
+++ responseBuilder.js
@@ -1,3 +1,20 @@
-function buildHttpResponse(request) {
+const router = require('./router');
 
+function buildHttpResponse(request) {
+    const route = router(request);
+    let res = [];
+
+    res.push("HTTP/1.1 " + route.statusCode + " " + route.statusMessage + "\r\n");
+
+    for (let key in route.headers) {
+        res.push(`${key}: ${route.headers[key]}\r\n`);
+    }
+
+    res.push('\r\n');
+
+    if (route.body) {
+        res.push(route.body);
+    }
+
+    return res.join('');
 };
---
+++ router.js
@@ -0,0 +1,50 @@
+function routerResponse(request) {
+    const path = request.path.toLowerCase();
+
+    if (path == "/home") {
+        return {
+            statusCode: 200,
+            statusMessage: 'OK',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: 'Welcome to my personal portfolio!'
+        };
+    } else if (path == "/about") {
+        return {
+            statusCode: 200,
+            statusMessage: 'OK',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: 'Read this page to learn more about me!'
+        };
+    } else if (path == "/projects") {
+        return {
+            statusCode: 200,
+            statusMessage: 'OK',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: 'This page includes projects I have worked on.'
+        };
+    } else if (path == "/contact") {
+        return {
+            statusCode: 200,
+            statusMessage: 'OK',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: 'Contact me at natedem@stanford.edu'
+        };
+    } else {
+        return {
+            statusCode: 404,
+            statusMessage: 'Not Found',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: 'Page not found.'
+        };
+    }
+};
